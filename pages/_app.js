eb1c5207f3f62a759e787523543180fa99955519
Raul99GC <raulc99g@gmail.com>
2022-12-12 22:01:33 -0600

    first commit, add prop-types in dependencies and eslint with standard in devDependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import React from 'react'
+import Proptypes from 'prop-types'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+MyApp.propTypes = {
+  Component: Proptypes.any,
+  pageProps: Proptypes.any
+}
