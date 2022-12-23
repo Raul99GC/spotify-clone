3d7a5c06fa7e0b4cbd62efe4407596cf59a1dc83
Raul99GC <raulc99g@gmail.com>
2022-12-22 21:09:19 -0600

    adding the colors for the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        spoify: {
+          green: '#1DB954 ',
+          black: '#000000',
+          white: '#FFFFFF',
+          darkestGray: '#121212',
+          gray: '#282828',
+          lighGray: '#B3B3B3'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
