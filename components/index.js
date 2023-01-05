c6186984d8f0fdd5bc0deaf2d3360e08e7ce2e37
Raul99GC <raulc99g@gmail.com>
2023-01-04 19:52:57 -0600

    changing the routes of components and adding more too

--- components/index.js
+++ components/index.js
@@ -1,2 +1,3 @@
-export { default as Header } from './Header'
-export { default as Sidebar } from './Sidebar'
+export { default as Header } from './Header/Header'
+export { default as NavMobile } from './NavMobile/NavMobile'
+export { default as Sidebar } from './Sidebar/Sidebar'
