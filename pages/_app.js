d292262103b981a9c4d15ed34b08cc4b65c33458
Raul99GC <raulc99g@gmail.com>
2023-01-04 23:17:28 -0600

    modifying MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,15 @@ import Proptypes from 'prop-types'
 import '../styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="bg-spoify-darkestGray min-h-screen text-gray-300">
+        <main className='snap-none'>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default MyApp
