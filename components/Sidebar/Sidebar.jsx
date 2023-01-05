318d9e720051b27fb5851bcfeb0524e007f90f0c
Raul99GC <raulc99g@gmail.com>
2023-01-04 19:46:03 -0600

    changing the route and fhinished all the component

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import spotifyImage from '../public/spotify-logo-img.png'
+import spotifyImage from '../../public/spotify-logo-img.png'
 
 import { AiFillHome, AiFillHeart } from 'react-icons/ai'
 import { BsSearch } from 'react-icons/bs'
