66505e4a62ee0407dae52056904ee9b64e2fbac0
Raul99GC <raulc99g@gmail.com>
2023-03-16 07:58:06 -0600

    adding the Header, Sidebar if the page is mobile or desktop

--- pages/_app.js
+++ pages/_app.js
@@ -5,15 +5,23 @@ import store from '../store'
 
 import '../styles/globals.css'
 
+import { useRouter } from 'next/router'
+import { Header, Sidebar } from '../components'
+import { IsMobile } from '../components/IsMobile/IsMobile'
+
 function MyApp ({ Component, pageProps }) {
+  const router = useRouter()
+  const { pathname } = router
+  const isLoginPage = pathname === '/login'
   return (
-    <>
-      <Provider store={store}>
-        <div className="bg-spoify-darkestGray min-h-screen text-gray-300 overflow-hidden">
-          <Component {...pageProps} />
-        </div>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <div className="bg-spoify-darkestGray min-h-screen text-gray-300 overflow-hidden">
+      <IsMobile />
+        {!isLoginPage && <Header />}
+        {!isLoginPage && <Sidebar />}
+        <Component {...pageProps} />
+      </div>
+    </Provider>
   )
 }
 
